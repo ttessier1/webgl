1e4139bb2530776f8097cae556c233d0f6c8f6b1
Tim Tessier <flieingzbu@gmail.com>
2024-11-09 09:37:59 -0700

    Tutorial 4 A

---
+++ tutorial4/index.php
@@ -0,0 +1,86 @@
+<html>
+	<head>
+		<title>WebGl Samples</title>
+		<link rel="stylesheet" href="../css/main.css" />
+		<script type="text/javascript" src="../js/main.js"></script>
+		<script type="text/javascript" src="js/tutorial.js"></script>
+		<script id="vertex-shader-1" type="notjs">
+attribute vec2 a_position;
+attribute vec2 a_texCoord;
+
+uniform vec2 u_resolution;
+
+varying vec2 v_texCoord;
+
+void main() {
+   // convert the rectangle from pixels to 0.0 to 1.0
+   vec2 zeroToOne = a_position / u_resolution;
+
+   // convert from 0->1 to 0->2
+   vec2 zeroToTwo = zeroToOne * 2.0;
+
+   // convert from 0->2 to -1->+1 (clipspace)
+   vec2 clipSpace = zeroToTwo - 1.0;
+
+   gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+
+   // pass the texCoord to the fragment shader
+   // The GPU will interpolate this value between points.
+   v_texCoord = a_texCoord;
+}
+		</script>
+		<script id="fragment-shader-1" type="notjs">
+precision mediump float;
+
+// our texture
+uniform sampler2D u_image;
+
+// the texCoords passed in from the vertex shader.
+varying vec2 v_texCoord;
+
+void main() {
+   gl_FragColor = texture2D(u_image, v_texCoord);
+}
+		</script>
+
+		<script id="fragment-shader-2" type="notjs">
+precision mediump float;
+
+// our texture
+uniform sampler2D u_image;
+
+// the texCoords passed in from the vertex shader.
+varying vec2 v_texCoord;
+
+void main() {
+   gl_FragColor = texture2D(u_image, v_texCoord).bgra;
+}
+
+
+		</script>
+	</head>
+	<body>
+		<div class="tab">
+			<button id="btnCanvas" class="tabLinks">Canvas</button>
+			<button id="btnHtml" class="tabLinks">Html</button>
+			<button id="btnJavascript" class="tabLinks">Javascript</button>
+			<button id="btnCss" class="tabLinks">Css</button>
+		</div>
+		<div id="Canvas" class="tabContent">
+			<h3>Canvas</h3>
+			<canvas id="canvs" width="800" height="600"></canvas>	
+		</div>
+		<div id="Html" class="tabContent">
+			<h3>Html</h3>
+			<iframe width="100%" height="600" src="https://www.swhistlesoft.com/demo/paste/paste-16/abcdefabcdefabcdefababcdefabcdef"></iframe>	
+		</div>
+		<div id="Javascript" class="tabContent">
+			<h3>Javascript</h3>
+			<iframe width="100%" height="600" src="https://www.swhistlesoft.com/demo/paste/paste-15/abcdefabcdefabcdefababcdefabcdef"></iframe>	
+		</div>
+		<div id="Css" class="tabContent">
+			<h3>Css</h3>
+			<iframe width="100%" height="600" src="https://www.swhistlesoft.com/demo/paste/paste-14/abcdefabcdefabcdefababcdefabcdef"></iframe>	
+		</div>
+	</body>
+</html>
